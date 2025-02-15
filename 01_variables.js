867890b4707c2f938bfee23344fe889e1fff9e13
Abhijit Mondal <abhijitamp378@gmail.com>
2025-02-15 20:31:33 +0530

    Add files via upload

---
+++ 01_variables.js
@@ -0,0 +1,21 @@
+const accountId = "121123"
+let accountEmail = "abhijit@gmail.com"
+var accountPassword = "2135"
+accountCity = " kolkata"
+let accountState ;
+
+// accountId = 2 // not allowed
+
+accountEmail = "abhi@gmail.com"
+accountPassword = "35645"
+accountCity = "ahmadpur"
+
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
+
+
+
+
+
+//for declaring constant in js we have to use const ; 
+// and for declaring variable there is 2 methods - let,var ; 
+// but prefer not to use var because of issue in block scope and functional scope 
---
+++ 02_datatypes.js
@@ -0,0 +1,23 @@
+ "use strict"; // treat all js code as newer version 
+//  alert (3+3)// we are using nodejs, not browser
+console.log(3+
+    3
+) // cose readability should be higher preference 
+ console.log("abhi")
+
+ let name = "abhi"
+ let age =21
+ let isloogedIn = false;
+
+ // number => range : 2 to the power 53
+ // bigint
+ //string =>""
+ //boolean => true/false
+ // null => standalone value 
+ // undefined => 
+    //symbol => unique
+ //object => 
+
+
+    console.log(typeof null); // object 
+    console.log(typeof undefined) // undefined
